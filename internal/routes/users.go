4b15339667eff9e5e8f358bd85d2160df33a306d
Amardeep Ranjan <amardeepranjan911@gmail.com>
2024-11-16 12:26:00 +0530

    feat: routes added

--- internal/routes/users.go
+++ internal/routes/users.go
@@ -7,6 +7,23 @@ import (
 
 // RegisterRoutes sets up the routes for the application
 func RegisterUserRoutes(e *echo.Echo) {
-	e.POST("/api/google-signup", handlers.GoogleSignup)
+	e.POST("/api/signup", handlers.Signup)
+	e.POST("/api/verify-otp", handlers.VerifyOTP)
+	e.POST("/api/resend-otp", handlers.ResendOTP)
+	e.POST("/api/login", handlers.Login)
+	e.POST("/api/forgot-password", handlers.ForgotPassword)
+	e.POST("/api/resend-forgot-otp", handlers.ResendForgotOTP)
+	e.POST("/api/verify-forgot-otp", handlers.ForgotVerifyOTP)
+	e.POST("/api/change-password-unauthenticated", handlers.ChangePasswordUnauthenticated)
+	e.POST("/api/change-password-authenticated", handlers.ChangePasswordAuthenticated)
 	e.POST("/api/google-login", handlers.GoogleLogin)
+	e.POST("/api/google-signup", handlers.GoogleSignup)
+
+	// Admin APIs with `/api` prefix
+	e.GET("/api/get-all-users", handlers.GetAllUsers)
+	e.GET("/api/get-user", handlers.GetUser)
+	e.POST("/api/user", handlers.BlockUnblockUser)
+	e.GET("/api/blocked-user", handlers.BlockedUser)
+	e.GET("/api/get-all-blocked-users", handlers.GetAllBlockedUsers)
+	e.GET("/api/orders", handlers.GetOrdersByUserID)
 }

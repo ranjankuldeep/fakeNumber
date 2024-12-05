b9e0fcff9202d11b9fee4a02fc81d6d6e5a4bc3e
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-12-06 01:44:33 +0530

    feat: fixes vcs false

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/gorilla/sessions"
@@ -21,40 +20,11 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
-func Load(envFile string) {
-	err := godotenv.Load(dir(envFile))
-	if err != nil {
-		panic(fmt.Errorf("Error loading .env file: %w", err))
-	}
-}
-func dir(envFile string) string {
-	currentDir, err := os.Getwd()
-	if err != nil {
-		panic(err)
-	}
-
-	for {
-		goModPath := filepath.Join(currentDir, "go.mod")
-		if _, err := os.Stat(goModPath); err == nil {
-			break
-		}
-
-		parent := filepath.Dir(currentDir)
-		if parent == currentDir {
-			panic(fmt.Errorf("go.mod not found"))
-		}
-		currentDir = parent
-	}
-
-	return filepath.Join(currentDir, envFile)
-}
-
 var (
 	store = sessions.NewCookieStore([]byte("mY FUckingSEcretKey"))
 )
 
 func main() {
-	Load(".env")
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatalf("Error loading .env file: %v", err)

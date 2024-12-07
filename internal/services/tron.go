752333b9cbf6681f2630bc23f3069e45d6c83ef5
kuldeep ranjan <ranjankuldeep19@gmail.com>
2024-12-07 18:31:02 +0530

    feat: changed url

--- internal/services/tron.go
+++ internal/services/tron.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/ranjankuldeep/fakeNumber/logs"
 )
 
 // TronAddressResponse represents the structure of the response from the Tron address API
@@ -13,23 +15,18 @@ type TronAddressResponse struct {
 	PrivateKey string `json:"privatekey"`
 }
 
-// GenerateTronAddress fetches a Tron address and private key from the given API
 func GenerateTronAddress() (string, string, error) {
 	apiURL := "https://php.paidsms.in/tron/?type=address"
-
-	// Make the HTTP GET request
 	resp, err := http.Get(apiURL)
 	if err != nil {
 		return "", "", fmt.Errorf("failed to fetch Tron address: %w", err)
 	}
 	defer resp.Body.Close()
 
-	// Check for a non-200 status code
 	if resp.StatusCode != http.StatusOK {
 		return "", "", fmt.Errorf("unexpected status code: %d", resp.StatusCode)
 	}
 
-	// Read the response body
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return "", "", fmt.Errorf("failed to read response body: %w", err)
@@ -40,7 +37,7 @@ func GenerateTronAddress() (string, string, error) {
 	if err := json.Unmarshal(body, &tronAddress); err != nil {
 		return "", "", fmt.Errorf("failed to parse JSON response: %w", err)
 	}
-
+	logs.Logger.Info(tronAddress)
 	// Return the address and private key
 	return tronAddress.PrivateKey, tronAddress.Address, nil
 }
